9c53c3f060c5db4c66be8b64539f17398201c584
searobea <searobea@gmail.com>
2019-05-16 09:35:00 -0500

    update

--- static/js/app.js
+++ static/js/app.js
@@ -5,14 +5,13 @@ var tableData = data;
 var tbody = d3.select("tbody");
 var button = d3.selectAll("#filter-btn");
 
-
+// filter data
 function formClick(){
   d3.event.preventDefault();
-  var datetimeEntry = d3.select("#datetime");
-  var datetimeValue = datetimeEntry.property("value");
-  var filteredData = tableData.filter(entry => entry.datetime === datetimeValue);
+  var formInput = d3.select("#search-criteria");
+  var inputValue = formInput.property("value");
+  var filteredData = tableData.filter(entry => entry.datetime === inputValue);
   
-  console.log(datetimeValue);
   console.log(filteredData);
 
   filteredData.forEach((sighting) => {
@@ -28,7 +27,21 @@ button.on("click", formClick);
 
 
 
-// d3.selectAll("#filter-btn").on("click", formClick);
 
+// all sightings
+var allButton = d3.select("#full-table");
+function showAll() {
+  d3.event.preventDefault();
+  var tbody = d3.select("tbody");
+  tableData.forEach((report) => {
+    var row = tbody.append("tr");
+    Object.entries(report).forEach(([key, value]) => {
+      var cell = tbody.append("td");
+      cell.text(value);
+    });
+  });
+}
+allButton.on("click", showAll);
 
-
+// clear table 
+d3.select("#clear-table").on("click",d3.event.default());
