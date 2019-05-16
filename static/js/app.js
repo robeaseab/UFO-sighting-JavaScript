711b07f2ce8bcdfd79f4d0da429dd7eefb12f528
searobea <searobea@gmail.com>
2019-05-16 12:23:39 -0500

    update

--- static/js/app.js
+++ static/js/app.js
@@ -5,14 +5,63 @@ var tableData = data;
 var tbody = d3.select("tbody");
 var button = d3.selectAll("#filter-btn");
 
-// filter data
+// filter data by date only
+// function formClick(){
+//   d3.event.preventDefault();
+//   var formInput = d3.select("#search-criteria");
+//   var inputValue = formInput.property("value");
+//   var filteredData = tableData.filter(entry => entry.datetime === inputValue);
+  
+//   console.log(filteredData);
+
+//   filteredData.forEach((sighting) => {
+//     var row = tbody.append("tr");
+//     Object.entries(sighting).forEach(([key, value]) => {
+//       var cell = tbody.append("td");
+//     cell.text(value);
+//     });
+//   });
+  
+//   }
+// button.on("click", formClick);
+
+// // .replace('"', "");
+// d3.event.preventDefault();
+
 function formClick(){
   d3.event.preventDefault();
+  // get value of drop down choice
+  var entryType = d3.select("#select-filter");
+  var entryValue = entryType.property("value");
+
+  // get value of search input
   var formInput = d3.select("#search-criteria");
   var inputValue = formInput.property("value");
-  var filteredData = tableData.filter(entry => entry.datetime === inputValue);
+  
+  // get data.entryValue
+  if (entryValue == "datetime") {
+    var filteredData = tableData.filter(entry => entry.datetime === inputValue);
+  }
+  else if (entryValue == "city") {
+    var filteredData = tableData.filter(entry => entry.city === inputValue);
+  }
+  else if (entryValue == "state") {
+    var filteredData = tableData.filter(entry => entry.state === inputValue);
+  }
+  else if (entryValue == "country") {
+    var filteredData = tableData.filter(entry => entry.country === inputValue);
+  }
+  else if (entryValue == "shape") {
+    var filteredData = tableData.filter(entry => entry.shape === inputValue);
+  }
+  else {
+    console.log("error");
+  }
+  
+  
   
   console.log(filteredData);
+  console.log(entryValue);
 
   filteredData.forEach((sighting) => {
     var row = tbody.append("tr");
@@ -24,6 +73,8 @@ function formClick(){
   
   }
 button.on("click", formClick);
+
+
 
 
 
